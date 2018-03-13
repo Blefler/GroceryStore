48f46962d389a141d42b388f5f02935b7a2eef58
Blefler <blefler@mail.csuchico.edu>
2018-03-13 16:48:07 -0700

    Updated from array to vector

--- main.cpp
+++ main.cpp
@@ -1,45 +1,48 @@
 //Brad Lefler
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
-  string food;
-do{
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+vector <string> list; 
+//int numItems = 0;
+char input;
+string food;
+
+do
+{
+cout<<"\n==GROCERY LIST MANAGER==";
+cout<<"\nENTER YOUR CHOICE: ";
+cout<<"\n (A)dd an item";
+cout<<"\n (Q)uit";
+cout<<"\nYour Choice (A/Q): ";
+cin>>input;
+
   if(input=='A'||input=='a')
   {
     cout<<"What is the item?"<<endl;
-    cin>>food; 
-     if(numItems<5)
-      {
-       list[numItems]=food;
-       numItems++;
-      }
-      else
-      {  
-        cout<<"You'll need a bigger list!"<<endl;
-      }
+    cin>>food;
+    list.push_back(food);
   }
-  }
-  while( input!='Q'&&input!='q' );
-  if(input=='Q'||input=='q')
+
+}
+while(input!='Q'&&input!='q');
+
+if(list.size()>0)
+{
+  for(int i=0; i < list.size(); i++)
   {
-    cout<<"==ITEMS TO BUY=="<<endl;
-    cout<<"1 "<<list[0]<<endl;
-    cout<<"2 "<<list[1]<<endl;
-    cout<<"3 "<<list[2]<<endl;
-    cout<<"4 "<<list[3]<<endl;
-    cout<<"5 "<<list[4]<<endl;
+  cout<<list[i]<<endl;
   }
-  return 0;
+}
+else
+  {
+  cout<<"No items to buy!"<<endl;
+  }
+
+
+
+return 0;
 }
